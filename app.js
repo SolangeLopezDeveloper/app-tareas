d7846e6fc2eb3da59b33fac5f8f62d31297da55a
SolangeLopezDev <solangelopezdev@gmail.com>
2022-11-04 09:31:46 -0300

    Aplicacion Tareas Terminada

---
+++ app.js
@@ -0,0 +1,68 @@
+const process = require('process');
+const moduloTareas = require('./funcionesDeTareas');
+
+
+const accion = process.argv[2];
+
+switch (accion) {
+    case "listar":
+        const tareasArray = moduloTareas.leerArchivo();
+        console.log("-------------Listado de tareas -------------------"); 
+        tareasArray.forEach((tarea, index) => {
+        console.log(`${tareasArray[index].titulo} : ${tareasArray[index].estado}`)
+        });
+        break;
+
+        case "detalle" : 
+        titulo = process.argv[3];
+        console.log(moduloTareas.detalleTareas(titulo));
+        break;
+
+
+    case "crear" :
+    {
+            titulo = process.argv[3],
+            estado = "pendiente"
+    }
+        console.log(moduloTareas.guardarTarea(titulo, estado));    
+        break;   
+        
+
+    case "filtrar" :
+        estado = process.argv[3];
+console.log(moduloTareas.filtrarPorEstado(estado));
+        break;
+
+        case "editar":
+            titulo = process.argv[3];
+            estado = process.argv[4];
+
+            console.log(moduloTareas.editarTarea(titulo,estado));
+            break;
+
+            case "eliminar":
+                titulo = process.argv[3];
+                console.log(moduloTareas.eliminarTarea(titulo));
+                break;
+
+    case undefined:
+        console.log();
+        console.log("Atención - Tienes que pasar una acción.")
+        console.log("Las acciones disponibles son: ");
+        console.log("-listar");
+        console.log("-editar");
+        console.log("-crear");
+        console.log("-eliminar");
+        console.log("-filtrar");
+        break;
+
+    default:
+        console.log("No entiendo qué quieres hacer.");
+        console.log("Las acciones disponibles son: ");
+        console.log("-listar");
+        console.log("-editar");
+        console.log("-crear");
+        console.log("-eliminar");
+        console.log("-filtrar");
+
+}  
---
+++ funcionesDeTareas.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+
+
+const moduloTareas = {
+    tareasJSON: 'tareas.json',
+    leerArchivo: function () {
+    
+           return JSON.parse(fs.readFileSync(this.tareasJSON, 'utf-8'));
+
+    },
+    
+    escribirJSON : function(tareasArray){
+        fs.writeFileSync((this.tareasJSON),JSON.stringify(tareasArray,null,3), 'utf-8')
+        return 'Tareas Actualizadas!'
+    },
+
+    guardarTarea : function (tarea){
+        let tareasArray = this.leerArchivo();
+         let nuevaTarea = {
+            titulo : titulo.trim(),
+            estado : "pendiente"
+        }
+
+        tareasArray.push(nuevaTarea);
+        this.escribirJSON(tareasArray)
+        return "Tarea guardada con éxito!!!"
+    },
+detalleTareas : function(titulo){
+
+    const tareas = this.leerArchivo();
+        let tarea = tareas.find(tarea => {
+            return tarea.titulo === titulo
+})
+return tarea ? tarea : "No hay tareas con el titulo :" + titulo;
+},
+
+    filtrarPorEstado : function (estado){
+         if(!estado){
+            return "Debes escribir un estado para la búsqueda!"};
+
+            const tareas = this.leerArchivo();
+        const estadoFiltrado = tareas.filter(tarea => {
+                return tarea.estado.toLowerCase().includes(estado.toLowerCase())
+            });
+            return estadoFiltrado.length ? estadoFiltrado : "No existe el estado " + estado
+        },
+
+        editarTarea : function(titulo, estado){
+            let tareas = this.leerArchivo();
+
+            if(!titulo){
+                return "Se requiere el titulo de la tarea que deseas editar!"
+            }
+let tareaEditada = {};
+            let tareasEditadas = tareas.map(tarea => {
+                if (tarea.titulo === titulo){
+
+                      tareaEditada = {
+                          titulo: tarea.titulo,
+                          estado: estado ? estado : tarea.estado
+}
+ return tareaEditada
+                
+                } return tarea 
+            })
+            this.escribirJSON(tareasEditadas)
+            return tareasEditadas
+        },
+
+        eliminarTarea : function(titulo){
+            if(!titulo){
+                return "Se requiere el titulo de la tarea a eliminar!"
+            }
+            let tareas = this.leerArchivo();
+            const tareasExistentes = tareas.filter(tarea=> tarea.titulo !== titulo);
+            this.escribirJSON(tareasExistentes);
+            return tareasExistentes;
+        }
+    }
+
+
+    
+
+
+
+module.exports = moduloTareas;
+
